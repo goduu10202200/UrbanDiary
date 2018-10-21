f033406cf8822c49d16b961c5b20ac0a2b3911e9
goduu10202200 <goduu10202200@gmail.com>
2018-10-22 00:04:19 +0800

    1. 刪除2個新增文件 2. ud_backend 改為 ud_api(api感覺比較厲害) 3. 增加sql資料夾、增加.sql檔案

---
+++ sql/ud_1022.sql
@@ -0,0 +1,114 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.4
+-- https://www.phpmyadmin.net/
+--
+-- 主機: 127.0.0.1
+-- 產生時間： 2018 年 10 月 21 日 18:01
+-- 伺服器版本: 10.1.29-MariaDB
+-- PHP 版本： 7.2.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- 資料庫： `ud`
+--
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `member`
+--
+
+CREATE TABLE `member` (
+  `id` int(11) NOT NULL COMMENT '編號',
+  `username` varchar(50) NOT NULL COMMENT 'e-mail',
+  `name` varchar(10) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- 資料表的匯出資料 `member`
+--
+
+INSERT INTO `member` (`id`, `username`, `name`, `created_at`, `updated_at`) VALUES
+(1, 'goduu10202200@gmail.com', 'value2', '2018-10-21 15:28:21', '2018-10-21 15:28:21');
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `scheduled`
+--
+
+CREATE TABLE `scheduled` (
+  `id` int(11) NOT NULL COMMENT '編號',
+  `username` int(11) NOT NULL COMMENT '連結member.username',
+  `content` varchar(500) NOT NULL COMMENT '內容',
+  `type` varchar(20) NOT NULL COMMENT '類別',
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- 資料表的匯出資料 `scheduled`
+--
+
+INSERT INTO `scheduled` (`id`, `username`, `content`, `type`, `created_at`, `updated_at`) VALUES
+(1, 1, 'eeeeeee', '', '2018-10-21 15:53:11', '2018-10-21 15:53:11');
+
+--
+-- 已匯出資料表的索引
+--
+
+--
+-- 資料表索引 `member`
+--
+ALTER TABLE `member`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- 資料表索引 `scheduled`
+--
+ALTER TABLE `scheduled`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `usernameToid` (`username`);
+
+--
+-- 在匯出的資料表使用 AUTO_INCREMENT
+--
+
+--
+-- 使用資料表 AUTO_INCREMENT `member`
+--
+ALTER TABLE `member`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '編號', AUTO_INCREMENT=2;
+
+--
+-- 使用資料表 AUTO_INCREMENT `scheduled`
+--
+ALTER TABLE `scheduled`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '編號', AUTO_INCREMENT=2;
+
+--
+-- 已匯出資料表的限制(Constraint)
+--
+
+--
+-- 資料表的 Constraints `scheduled`
+--
+ALTER TABLE `scheduled`
+  ADD CONSTRAINT `scheduled_ibfk_1` FOREIGN KEY (`username`) REFERENCES `member` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
