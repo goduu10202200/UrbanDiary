5f3db0f0bc7b6a80e09bbed8f9869180f05e68db
sandy010924 <sandy010924@gmail.com>
2018-10-27 03:23:19 +0800

    no message

--- ud/navigation/AppNavigator.js
+++ ud/navigation/AppNavigator.js
@@ -9,35 +9,37 @@ import Signup from "../screens/Signup";
 
 import History_year from "../screens/History_year";
 import History_day from "../screens/History_day";
+import Add_location from "../screens/Add_location";
 
 // import MapDiary from "../screens/MapDiary";
+    }, {
+  const RootStack = createStackNavigator({
+    Main: {
+      screen: MainTabNavigator,
+      navigationOptions: {
+        header: null //去除上方title
+      }
+    },
+    Enter: {
+      screen: Enter,
+      navigationOptions: {
+        header: null //去除上方title
+      }
+    },
+    Signup: Signup,
+    Login: Login,
+    History_year: History_year,
+    History_day: History_day,
+    Add_location: Add_location
+  }, {
+      initialRouteName: 'Main',
+    });
 
-const RootStack = createStackNavigator({
-  Main: {
-    screen: MainTabNavigator,
-    navigationOptions: {
-      header: null //去除上方title
+  export default class App extends React.Component {
+    render() {
+      return <RootStack />;
     }
-  },
-  Enter: {
-    screen: Enter,
-    navigationOptions: {
-      header: null //去除上方title
-    }
-  },
-  Signup: Signup,
-  Login: Login,
-  History_year: History_year,
-  History_day: History_day
-},{
-  initialRouteName: 'Main',
-});
-
-export default class App extends React.Component {
-  render() {
-    return <RootStack />;
   }
-}
 
 
 /*
