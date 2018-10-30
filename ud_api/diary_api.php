450a7d82cc1dcf3b803e3ed2bfcd60bbdf431ae3
goduu10202200 <goduu10202200@gmail.com>
2018-10-30 23:03:22 +0800

    1. 資料庫備份(diary 資料表新增 date 欄位 、 jieba 資料表新增 diary_id) 2. diary api (新增date)

--- ud_api/diary_api.php
+++ ud_api/diary_api.php
@@ -3,19 +3,15 @@
     $raw_post_data = json_decode(file_get_contents('php://input'), true);
     
     $content =  $raw_post_data['content'];
-    $today = date("Y-m-d H:i:s");
+    $today = date("Y-m-d");
 
-    $sql = "INSERT INTO diary (username, content, created_at)
+    $sql = "INSERT INTO diary (username, content, date)
     VALUES (1, '".$content."', '".$today."')";
 
     if ($conn->query($sql) === true) {
         echo "New record created successfully";
         header('Location: http://172.20.10.2:5000/jieba');
         exit;
-    // $url = "localhost:5000/jieba";
-        // echo "<script type='text/javascript'>";
-        // echo "window.location.href='$url'";
-        // echo "</script>";
     } else {
         echo "Error: " . $sql . "<br>" . $conn->error;
     }
