79aa2d42ee4e8c651f7399ddf0c60065ecf3e0f0
goduu10202200 <goduu10202200@gmail.com>
2018-12-06 21:23:17 +0800

    1. 上傳圖片路徑

--- ud/screens/ServiceApiNet.js
+++ ud/screens/ServiceApiNet.js
@@ -1,6 +1,9 @@
 // const apiURL = "http://192.168.0.13/ud/api/ud/";
-const apiURL = "http://172.20.10.2/ud/api/ud/";
-const uploadURL = "http://172.20.10.2/ud/view/ud/assets/uploadfile/";
+// const apiURL = "http://172.20.10.2/ud/api/ud/";
+// const uploadURL = "http://172.20.10.2/ud/view/ud/assets/uploadfile/";
+const apiURL = "http://172.20.10.3:8181/ud/api/ud/";
+const uploadURL = "http://172.20.10.3:8181/ud/assets/uploadfile/images/";
+
 export default {
   getURL() {
     return apiURL;
