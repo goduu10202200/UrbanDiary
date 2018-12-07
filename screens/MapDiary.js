3009b92601dc378e1d3e4a6728febae6fd0fa4b2
goduu10202200 <goduu10202200@gmail.com>
2018-12-07 23:24:00 +0800

    1. 更改地圖樣式

--- screens/MapDiary.js
+++ screens/MapDiary.js
@@ -62,13 +62,14 @@ export default class MapDiary extends React.Component {
   }
 
   render() {
-    this.marker();
+    // this.marker();
     return (
       <View style={styles.container}>
         <MapView
-          provider={this.props.provider}
+          provider={PROVIDER_GOOGLE}
           style={styles.map}
           initialRegion={this.state.region}
+          customMapStyle={mapStyle}
         >
           {this.state.markers === "No data"
             ? null
@@ -86,6 +87,7 @@ export default class MapDiary extends React.Component {
                 />
               ))}
         </MapView>
+        //{" "}
       </View>
     );
   }
@@ -98,7 +100,11 @@ const styles = StyleSheet.create({
     alignItems: "center"
   },
   map: {
-    ...StyleSheet.absoluteFillObject
+    position: "absolute",
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0
   },
   bubble: {
     backgroundColor: "rgba(255,255,255,0.7)",
@@ -122,3 +128,157 @@ const styles = StyleSheet.create({
     backgroundColor: "transparent"
   }
 });
+
+var mapStyle = [
+  {
+    featureType: "administrative",
+    elementType: "labels.text.fill",
+    stylers: [
+      {
+        color: "#6195a0"
+      }
+    ]
+  },
+  {
+    featureType: "landscape",
+    elementType: "labels.text.fill",
+    stylers: [
+      {
+        color: "#328CB9"
+      }
+    ]
+  },
+  {
+    featureType: "landscape",
+    elementType: "geometry.fill",
+    stylers: [
+      {
+        color: "#ffffff"
+      }
+    ]
+  },
+  {
+    featureType: "poi",
+    elementType: "all",
+    stylers: [
+      {
+        visibility: "on"
+      }
+    ]
+  },
+
+  {
+    featureType: "poi.park",
+    elementType: "geometry.fill",
+    stylers: [
+      {
+        color: "#e6f3d6"
+      },
+      {
+        visibility: "on"
+      }
+    ]
+  },
+  {
+    featureType: "road",
+    elementType: "all",
+    stylers: [
+      {
+        saturation: -100
+      },
+      {
+        lightness: 45
+      },
+      {
+        visibility: "simplified"
+      }
+    ]
+  },
+  {
+    featureType: "road.highway",
+    elementType: "all",
+    stylers: [
+      {
+        visibility: "simplified"
+      }
+    ]
+  },
+  {
+    featureType: "road.highway",
+    elementType: "geometry.fill",
+    stylers: [
+      {
+        color: "#f4d2c5"
+      },
+      {
+        visibility: "simplified"
+      }
+    ]
+  },
+  {
+    featureType: "road.highway",
+    elementType: "labels.text",
+    stylers: [
+      {
+        color: "#4e4e4e"
+      }
+    ]
+  },
+  {
+    featureType: "road.arterial",
+    elementType: "geometry.fill",
+    stylers: [
+      {
+        color: "#f4f4f4"
+      }
+    ]
+  },
+  {
+    featureType: "road.arterial",
+    elementType: "labels.text.fill",
+    stylers: [
+      {
+        color: "#787878"
+      }
+    ]
+  },
+  {
+    featureType: "road.arterial",
+    elementType: "labels.icon",
+    stylers: [
+      {
+        visibility: "on"
+      }
+    ]
+  },
+  {
+    featureType: "transit",
+    elementType: "all",
+    stylers: [
+      {
+        visibility: "off"
+      }
+    ]
+  },
+  {
+    featureType: "water",
+    elementType: "all",
+    stylers: [
+      {
+        color: "#eaf6f8"
+      },
+      {
+        visibility: "on"
+      }
+    ]
+  },
+  {
+    featureType: "water",
+    elementType: "geometry.fill",
+    stylers: [
+      {
+        color: "#eaf6f8"
+      }
+    ]
+  }
+];
