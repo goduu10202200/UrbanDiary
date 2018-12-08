9f9a357d8d7efbd01db9f7fb0dceb8b68caed69e
goduu10202200 <goduu10202200@gmail.com>
2018-12-08 13:38:12 +0800

    1. 更改APP 上面的top bar 白色底線

--- screens/style/style_layout.js
+++ screens/style/style_layout.js
@@ -1,59 +1,59 @@
 var styles_layout = {
   container: {
     flex: 1,
-    backgroundColor: '#ededed',
-    justifyContent: 'center',
+    backgroundColor: "#ededed",
+    justifyContent: "center"
   },
   titleDiv: {
     height: 40,
-    backgroundColor: '#3b5998',
-    //borderWidth: 0,
+    backgroundColor: "#3b5998",
+    borderBottomWidth: 0
   },
   titleTxt: {
     width: "90%",
     fontSize: 18,
-    textAlign: 'center',
-    alignSelf: 'center',
+    textAlign: "center",
+    alignSelf: "center",
     color: "#fff",
-    letterSpacing: 1,
+    letterSpacing: 1
   },
   titleLogo: {
-    width: '50%',
-    height: '60%',
-    alignSelf: 'center',
-    resizeMode: 'contain',
-    marginTop: 5,
+    width: "50%",
+    height: "60%",
+    alignSelf: "center",
+    resizeMode: "contain",
+    marginTop: 5
   },
   titleSubmit: {
     fontSize: 16,
     color: "#0072d6",
-    marginRight: 5,
+    marginRight: 5
   },
   titleSubmit_2: {
     width: 15,
     height: 15,
     marginRight: 18,
-    marginBottom: 2,
+    marginBottom: 2
   },
   titleSubmit_3: {
     width: 23,
     height: 23,
     marginLeft: 15,
-    marginBottom: 1,
+    marginBottom: 1
   },
   titleIcon: {
     fontSize: 20,
     marginRight: 15,
     color: "#fff",
-    alignSelf: "center",
+    alignSelf: "center"
   },
   pagetitle: {
     fontSize: 30,
-    color: 'rgba(96,100,109, 1)',
+    color: "rgba(96,100,109, 1)",
     lineHeight: 50,
     marginTop: 30,
-    textAlign: 'center',
-  },
+    textAlign: "center"
+  }
 };
 
 export default styles_layout;
